c4e8658f4fc264286cb532b066a5a4e790000b13
LDAOnline <ldadams2005@gmail.com>
2025-05-25 13:53:06 +0100

    Added user authorization

--- FirstAuthenticationBasics/Controllers/HomeController.cs
+++ FirstAuthenticationBasics/Controllers/HomeController.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FirstAuthenticationBasics.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult UsersOnly()
         {
             return View();
         }
